f8152aa9f61c40ae6270c62af0a4a2fc37c9ceb6
aihon <34867245+aihon@users.noreply.github.com>
2017-12-26 21:03:26 +0100

    Update excercise8.R

--- chapter2/excercise8.R
+++ chapter2/excercise8.R
@@ -7,19 +7,20 @@
 
 # Load libraries
 library(haven)
+library(forecast)
 
 # Import the dataset and visualize it
-Dataset <- read_sas("/your_path/sim_2.sas7bdat")
-View(Dataset)
+dataset <- read_sas("/your_path/sim_2.sas7bdat")
+View(dataset)
 
 # Take only the data you need
-Dataset[3:4] <- list(NULL) 
+dataset[3:4] <- list(NULL) 
 
 # Plot the sequence Y1 against time. It seems  weakly stationary. In fact it fluctuates around a constant mean and the 
 # variability appears to be regular.
-Y1 <- Dataset[, 2]
-time <- Dataset[, 1]
-plot(time,Y1, type = "l")
+y1 <- dataset[, 2]
+time <- dataset[, 1]
+plot(time,Y1,type = "l")
 
 # Try to spot the stationarity of Y1 by looking at the ACF and PACF. Both go to 0 very quickly. This tells us that the series
 # can be stationary. (The blue lines give the values beyond which the autocorrelations are statistically significantly different
@@ -27,8 +28,8 @@ plot(time,Y1, type = "l")
 # From the sample ACF and PACF you can also guess that the DGP is an AR(1). That's because in the ACF the autocorrelations go to
 # 0 slowly and the first autocorreltions are significantly different from 0.
 # In the PACF only the first partial autocorrelation is signifacntly different from 0. 
-acf(x = Y1, lag.max = 30)
-pacf(x = Y1, lag.max = 30)
+acf(x = Y1,lag.max = 30)
+pacf(x = Y1,lag.max = 30)
 
 # Now we look if an AR(1) model fit the data well or not. We estimate an AR(1) without intercept: 
 #                                              y_(t) = a_(1) * y_(t-1) * e_(t)
@@ -37,35 +38,23 @@ pacf(x = Y1, lag.max = 30)
 # The s.e. of the intercept is big. Moreover, the AIC of the AR(1) without intercept is lower than the AIC of the AR(1) with
 # the intercept, then we "choose" the AR(1) without intercept.
 
-library(forecast)
-ARwoi <- Arima(x, c(1,0,0), include.mean = FALSE) #senza intercetta
-ARwi <- Arima(x, c(1,0,0))
+ARwoi  <- Arima(Y1,c(1,0,0),include.mean = FALSE) # AR(1) without intercept
+ARi <- Arima(Y1,c(1,0,0))
 ARwoi
-ARwi
+ARi
 
-
-# Si scarta l'AR(1) con intercetta.
-# Si procede ad una analisi dei residui
-# per vedere se è rimasta in giro dell'informazione
-# non catturata dal modello, ovvero se i residui sono white noise.
-# Dalla ACF i residui sembrano whithe noise, quindi il modello
-# fitta bene i dati. Perchè i residui devono essere withe noise/
-#l'acf deve mostrare incorrelazione se un modello è ben specificato?:
-# "It means you managed to extract all the information there is to extract.
-# What's left is just random and not representable by a model. 
-# If there is correlation in the residuals this regularity 
-# can be further used to extract some more information."
-# In alternativa usa i residui standardizzati, e plotta il grafico
-# contro il tempo e vedi se stanno nell banda +2,-2 
-
-Residui_woi <- residuals(ARwoi) 
+# We procede to do residual analysis to check if some information of the process is not captured by our model, that is,
+# we check if the residuals are white noise (we hope yes).
+# From ACF of residuals they seems white noise and so we can assert that our model fit well the data.
+# The following test is an explanation of why we want residuals to be white noise:
+# "It means you managed to extract all the information there is to extract. What's left is just random and not representable
+# by a model. If there is correlation in the residuals this regularity can be further used to extract some more information."
+# (In alternative we can plot the standardized residuals and look if they stay into the +2,-2 band of cofidence.
+residualsARwoi <- residuals(ARwoi) 
 acf(Residui_woi) 
 
-
-# Si usa il Ljung-Box test per testare
-# se un certo numero di lag non sono differenti da zero,
-# ovvero non c'è autocorrelazione. 
-
+# We use the Ljung-Box test to test if a certain number of lags are different from 0, that is, if there isn't correlation between 
+# lags.
 Box.test(resid(ARwoi),type="Ljung",lag=8)
 Box.test(resid(ARwoi),type="Ljung",lag=16)
 Box.test(resid(ARwoi),type="Ljung",lag=24)
