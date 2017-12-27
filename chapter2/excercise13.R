a6ea0b77b8bb265eede16a127184e3aa4bf9a4f5
aihon <34867245+aihon@users.noreply.github.com>
2017-12-27 14:28:08 +0100

    Update excercise13.R

--- chapter2/excercise13.R
+++ chapter2/excercise13.R
@@ -19,104 +19,69 @@ View(indProd)
 # We generate the growth rate of the series as:
 #                                 indProd_{t} = log(indProd_{t}) − log(indprod_{t−1})
 
-indprod <- ts(indprod)
-plot(indprod)
+indProd <- ts(indProd)
+indProdLog = log(indProd)
+diffIndProdLog = diff(indProdLog, lag = 1,  differences = 1)
 
-indprodlog = log(indprod)
-plot(indprodlog)
+# We look at the ACF of the differencieted series. It seems an AR(1) process.
+acf(diffIndProdLog)
 
-logdiff = diff(indprodlog, lag = 1,  differences = 1)
-plot(logdiff)
+# We estimate an AR(1) with intercept.
+arOne <- Arima(diffIndProdLog, order = c(1,0,0), fixed=c(NA,NA))
+arOne # AIC=-1237.08    BIC=-1227.02
+coeftest(arOne)
 
-acf(logdiff)
+# We estimate an AR(||1,8||). From the AIC and the BIC we see that the AR(||1,8||) fit better than the AR(1).
+arOneEight <- Arima(diffIndProdLog, order = c(8,0,0), fixed=c(NA,0,0,0,0,0,0,NA,NA))
+arOneEight # AIC=-1241.39  BIC=-1227.98
 
-# Si stima un AR(1) con intercetta
+# We extract the unemployment rate
+unempRate <- dataset[,14]
+unempRate <- ts(urate)
+plot(unempRate)  # the mean may not be constat 
+acf(unempRate)   # Go to 0 not immediatley, then unempRate may not be stationary  
 
-AR_1 <- Arima(logdiff, order = c(1,0,0), fixed=c(NA,NA))
-AR_1 # AIC=-1237.08    BIC=-1227.02
-coeftest(AR_1)
-
-# Si stima ora un AR(||1,8||), e dalla aic e
-# dalla bic si vede che fitta meglio
-
-AR_18 <- Arima(logdiff, order = c(8,0,0), fixed=c(NA,0,0,0,0,0,0,NA,NA))
-AR_18 # AIC=-1241.39  BIC=-1227.98
-
-# Si usa unemp
-
-urate <- Dati[,14]
-urate <- ts(urate)
-
-plot(urate)  # La media potrebbe non essere costante 
-acf(urate)   # Cade a zero dopo un po' 
-             # Ci sta quindi che la ts non sia stazionaria
-
-# Si stima come un AR(2) con intercetta
-
-AR_2 <- Arima(urate, order = c(2,0,0), fixed=c(NA,NA,NA))
-AR_2 # AIC=19.08   BIC=32.51
+# We try to fit unempRate with an AR(2) with intercept.
+arTwo <- Arima(unempRate, order = c(2,0,0), fixed=c(NA,NA,NA))
+arTwo # AIC=19.08   BIC=32.51
 
 
-# Per il punto iii) si definisce l'eq omogenea.
-# Si trovano poi le radici caratteristichje che 
-# vengono complesse coniugate.
-# Poi siccome (a_2)^0.5 è minore di 1 il processo 
-# converge
+# We take the first difference of unempRate to make it stationary. Then we model it as an AR(1).
+diffUnempRate = diff(unempRate, lag=1, differences = 1)
+plot(diffUnempRate)
+arOneDiff = Arima(unempRate, order = c(1,0,0), fixed=c(NA,NA))
+arOneDiff # AIC=147.92   BIC=157.99
 
-
-# Si modella la differenza prima del processo
-# come un AR(1). 
-
-uratediff = diff(urate, lag=1, differences = 1)
-plot(uratediff)
-
-AR_1_diff = Arima(urate, order = c(1,0,0), fixed=c(NA,NA))
-AR_1_diff # AIC=147.92   BIC=157.99
-
-# Si usa CPICORE
-
-cpicore = Dati[,10]
+# We extract CPICORE
+cpicore = dataset[,10]
 cpicore = ts(cpicore)
 
-# Si forma dly_t = log(cpicore_t) − log(cpicore_t−1)
+# We form:
+#                     dl(cpicore_{t}) = log(cpicore_{t}) − log(cpicore_{t−1})
+# The ACF of the log difference doesn't go to 0 quickly. Then unempRate may not be first difference stationary.
+cpicoreLog = log(cpicore)
+cpicoreDiff = diff(cpicoreLog, lag=1, differences = 1)
+plot(cpicoreDiff)
+acf(cpicoreDiff)   
+pacf(cpicoreDiff)
 
-cpicorelog = log(cpicore)
-cpicorediff = diff(cpicorelog, lag=1, differences = 1)
+# We try to take the second difference.
+cpicoreDiffTwo = diff(cpicoreDiff, lag=1, differences = 1)
+plot(cpicoreDiffTwo)
+acf(cpicoreDiffTwo) # unempRate is second difference stationary 
+pacf(cpicoreDiffTwo)
 
-plot(cpicorediff)
-acf(cpicorediff)   
-pacf(cpicorediff)
+# We check if, for cpicoreDiffTwo, fit better an AR(1) or an MA(1).
+# From the AIC and the BIC we see that the in-sample forecast is more accurate the one of MA(1).
+arOneCpicoreDiffTwo = Arima(cpicorediff2, order = c(1,0,0), fixed=c(NA,NA))
+arOneCpicoreDiffTwo # AIC=-1999.41    BIC=-1989.37
+maOneCpicoreDiffTwo = Arima(cpicorediff2, order = c(0,0,1), fixed=c(NA,NA))
+maOneCpicoreDiffTwo # AIC=-2000.39   BIC=-1990.35
 
-# La ACF non cade a 0. C'è quindi il caso che  la serie 
-# sia non stazionaria. Si prova a ridifferenziare
+# We estimate fit an AR(2) for cpicoreDiff 
+arTwoCpicoreDiff  = Arima(cpicorediff, order = c(2,0,0), fixed=c(NA,NA,NA))
 
-cpicorediff2 = diff(cpicorediff, lag=1, differences = 1)
 
-plot(cpicorediff2)
-acf(cpicorediff2) # Ora è stazionaria la serie diff2
-pacf(cpicorediff2)
-
-# Si guarda se per diff2 fitta meglio un AR(1) o un MA(1)
-
-AR_1_cpidiff2 = Arima(cpicorediff2, order = c(1,0,0), fixed=c(NA,NA))
-AR_1_cpidiff2 # AIC=-1999.41    BIC=-1989.37
-
-MA_1_cpidiff2 = Arima(cpicorediff2, order = c(0,0,1), fixed=c(NA,NA))
-MA_1_cpidiff2 # AIC=-2000.39   BIC=-1990.35
-
-# Dalla aic e della bic si vede ce l'in-sample forecast è
-# più accurato quello dell'MA(1)
-
-# Si stima cpicorediff come un AR(2)
-
-AR_2_cpidiff = Arima(cpicorediff, order = c(2,0,0), fixed=c(NA,NA,NA))
-AR_2_cpidiff
-
-# Le seguenti non sono le forecast h=12 per cpicore
-# vanno trasformate ma non so come fare
-
-FORECASTAR = forecast(AR_2_cpidiff,h=12)$mean
-FORECASTMA = forecast(MA_1_cpidiff2,h=12)$mean 
 
 
 
