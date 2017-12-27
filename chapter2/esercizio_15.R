3b742fcfd5fb7ae4199e749ff85e3729f8f711ec
aihon <34867245+aihon@users.noreply.github.com>
2017-12-27 15:40:44 +0100

    Update esercizio_15.R

--- chapter2/esercizio_15.R
+++ chapter2/esercizio_15.R
@@ -1,60 +1,40 @@
-#-----------------------------#
+###############################
 #                             #
-#        ESERCIZIO 16         #
+#        Excercise 16         #
 #                             #
-#-----------------------------#  
+###############################  
 
-
-# Clean workspace
-
-rm(list = ls(all=TRUE))
-
-# Librerie
-
+# Load libraries
 library(forecast)
 library(haven)
 library(gdata)
 library(lmtest)
 library(qcc)
 
+# We import the dataset and we take only what we need.
+dataset <- read.xls("/your_path/y_break.38134409.xls", sheet=1, header = TRUE)
+yBreak = dataset[,2]
+yBreakTs = ts(yBreak)
+plot(yBreakTs, type="l")
 
+# We try to fit an AR(1) model.
+arOne = Arima(yBreakTs, order = c(1,0,0), fixed = c(NA,NA))
+arOne # AIC=473.16   BIC=482.19
 
-# Dati
-
-Dati <- read.xls("/home/giovanni/Scaricati/y_break.38134409.xls", sheet=1, header = TRUE)
-View(Dati)
-
-
-plot(Dati, type = "l")
-
-# Proviamo a modellizzare la serie come un AR(1)
-
-ybreak = Dati[,2]
-View(ybreak)
-ybreakts = ts(ybreak)
-View(ybreakts)
-
-# Le dummy variable ci consentono di catturare dei cambiamenti
-# che si suppone ci siano stati nel processo e ceh quindi la descrizione/
-# modellizzazione del processo deve essere diversa per tenere conto di
-# questi cambiamenti. Le variabili dummy assumono valore 1 o 0.
-# Nel nostro caso si crede ci sia stato un break nel periodo 101.
-# Si testa quindi se c'è stato un intercept e/o slope break usando
-# dummy variables. 
-# Per testare un break nell'intercetta uso una dummy D_t che ha valore
-# 0 prima di 101 e valore 1 da 101 in poi. Se il suo coefficiente è
-# significativo vuol dire che vi è stato un cambiamento (vado a sommare il valore 
-# a quello dell'intercetta).
-# Per testare un brak nella slope uso la dummy D_t*y_t-1 e vale il ragionamento di sopra.
-
-# Si replicano i dati del libro
-
-
-AR1 = Arima(ybreakts, order = c(1,0,0), fixed = c(NA,NA))
-AR1 # AIC=473.16   BIC=482.19
+# Dummy variables allow us to capture changes that we suppose have occurred in the process. If these changes are really occurred
+# the modelling of the process have to consider it.
+# Dummy variablese can assume values 0 or 1.
+# In our case we think that a break have occurred in the period 101.
+# We test if this break is an intercept and/or a slope break using dummy variables.
+# In order to test a break in the intercept we use a dummy d_{t} that have value 0 before period 101 and value 101 from 101 
+# onwards.
+# If the coefficient of d_{t} is signficant it means that a break occurred (we sum the value of the coefficient of d_{t}
+# to the value of the intercept).
+# Instead, in order to test a break in the slope we use a dummy the d_{t} multiplied by y_{t-1}. If the coefficient of d_{t} is 
+# signficant it means that a break in the slope occurred (we sum the value of the coefficient of d_{t} to the value of 
+# coefficient y_{t-1}).
 
 # Prima testo um break nell'intercetta
-# PROVA A CERCARE INFO SUL CHOW TEST CON R
 
 dummy = rep(1,150)
 dummy[1:100]=0
