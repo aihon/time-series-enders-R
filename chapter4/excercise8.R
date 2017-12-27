ef2c2d8315758b2582d14162aea9680f2db7b411
aihon <34867245+aihon@users.noreply.github.com>
2017-12-27 18:02:44 +0100

    Update excercise8.R

--- chapter4/excercise8.R
+++ chapter4/excercise8.R
@@ -1,13 +1,10 @@
-#-----------------#
-#   ESERCIZIO 8   #
-#-----------------#
+#########################
+#                       #
+#     Excercise 8       #
+#                       #
+######################### 
 
-# Clean workspace
-
-rm(list = ls(all=TRUE))
-
-# Pacchetti
-
+# Load libraries
 library(forecast)
 library(FinTS)
 library(haven)
@@ -25,84 +22,50 @@ library(reshape2)
 library(CADFtest)
 library(dynlm)
 
+# Import the dataset.
+dataset <- read_sas("/your_path/quarterly.sas7bdat")
 
-# Dati
+# Form the spread.
+spread = dataset[,5] - dataset[,3]
+spread = ts(spread)
+acfSpread = acf(spread)
+acfSpread
 
-Dati <- read_sas("/home/giovanni/Scrivania/SASDatasets/quarterly.sas7bdat")
-View(Dati)
+# We use the model estimated to perform the Dickey-Fuller test by the "CADFtest" function to check how many lags indicates the
+# AIC and the BIC
+cadfAic = CADFtest(spread, type=c("drift"), max.lag.y=12, criterion=c("AIC"))
+summary(cadfAic) # 9 lags
+cadfBic = CADFtest(spread, type=c("drift"), max.lag.y=12, criterion=c("BIC"))
+summary(cadfBic) # 1 lag
 
-# Si forma lo spread
-
-s = Dati[,5] - Dati[,3]
-s = ts(s)
-as = acf(s)
-as
-
-# Si stima il modello usato ep rfare DF test e si guarda
-# quanti lags ci indicano i vari criteri
-
-scadfa = CADFtest(s, type=c("drift"), max.lag.y=12, criterion=c("AIC"))
-summary(scadfa) # 9 lags
-
-scadfb = CADFtest(s, type=c("drift"), max.lag.y=12, criterion=c("BIC"))
-summary(scadfb) # 1 lag
-
-estimated <- dynlm(d(s) ~ L(s, 1) + L(d(s), 1) + L(d(s), 2) +  L(d(s), 3) + L(d(s), 4)
-                     + L(d(s), 5) + L(d(s), 6) + L(d(s), 7) + L(d(s), 8)) 
-summary(estimated)    # il gts method seleziona 8 lags. il nono è buono ad un alpha 0.1
+# We check how many lags select the general-to-specifc (GTS) method.
+estimatedModel <- dynlm(d(s) ~ L(s, 1) + L(d(s), 1) + L(d(s), 2) +  L(d(s), 3) + L(d(s), 4)
+                        + L(d(s), 5) + L(d(s), 6) + L(d(s), 7) + L(d(s), 8)) 
+summary(estimated)    # the GTS method select 8 lags. 
 
 
-# Proviamo a fare l'adf test usando i lag
-# selezionati dai vari metodi per vedere se
-# conta il lag length nel DF test
+# We try to perform the ADF (Augmented Dickey-Fuller) using the number of lags selected by the various methods in order to see
+#if the lag lenght is relevant in the Dickey-Fuller test
+adfOne = ur.df(spread, type = c("drift"), lags = 1)    # 1 lag,  t = -4.7505 
+adfNine = ur.df(spread, type = c("drift"), lags = 9)   # 9 lags, t = -4.7026 
+adfEight = ur.df(spread, type = c("drift"), lags = 8)  # 8 lags, t = -4.3657
 
-test1 = ur.df(s, type = c("drift"), lags = 1)  # 1 lag, t = -4.7505 
+# From summary(adfOne) we get the critical values. At the significance levels 1, 5 and 10 the critical values are, respectively:
+# -3.46, -2.88 e -2.57. Hence, in all cases we reject the null of unit root and so the lag lenght it's not relevant.
+summary(adfOne)
 
-test2 = ur.df(s, type = c("drift"), lags = 9) # 9 lags, t = -4.7026 
+# Point (b)
+summary(adfEight) # All values are in accords to the values of the book hence we reject the null of unit root and we can conclude 
+                  # that the spread is stationary
 
-test3 = ur.df(s, type = c("drift"), lags = 8) # 8 lags, t = -4.3657
-
-summary(test1)
-
-# I valori critici si ricavano da
-# summary(test1). All' 1, 5 e 10 sono 
-# rispettivamente: -3.46, -2.88 e -2.57
-# e quindi in tutti i casi si rifiuta la nulla
-# di radice unitaria e quindi non conta
-# il lag length
-
-# Punto b)
-
-summary(test3) # tornano tutti i valori
-               # Si rifiuta la nulla e quindi 
-               # lo spread è stazionario
-
-
-# Punto c)
-
+# Point (c)
 r5= Dati[,5]
 r5 = ts(r5)
+adfR5 = ur.df(r5, type = c("drift"), lags = 7)   
+summary(adfR5) # t =  -0.7849 hence r5 is not stationary
 
-testr5 = ur.df(r5, type = c("drift"), lags = 7)   
-summary(testr5) # t =  -0.7849 r5 non è quindi stazionario
-
-# Punto d)
-
+# Punto (d)
 bill= Dati[,3]
 bill = ts(bill)
-
-testbill = ur.df(bill, type = c("drift"), lags = 11)   
-summary(testbill) # t =  -1.3443 bill non è quindi stazionario
-
-# Punto e)
-
-# Un'ipotesi può essere che il tbill dipende
-# dal r5 o si muove insieme e varia "in ritardo" e quindi
-# lo spread è come la differenze prima di uno stesso
-# processo
-
-# ALtra ipotesi è che la parte non stazionaria
-# dei due processi è simile e quindi sottraendoli si 
-# elimina la non stazionarietà
-
-
+adfBill = ur.df(bill, type = c("drift"), lags = 11)   
+summary(adfBill) # t =  -1.3443 hence bill is not stationary 
