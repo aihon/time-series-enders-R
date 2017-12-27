dbf1d20c463a304144f959260c41a7633cab93ea
aihon <34867245+aihon@users.noreply.github.com>
2017-12-27 19:15:48 +0100

    Update and rename bn.R to in_chapter_excercises/hpfilter.R

--- in_chapter_excercises/hpfilter.R
+++ in_chapter_excercises/hpfilter.R
@@ -1,16 +1,11 @@
-#---------------------------
-#
-#  ESERCIZIO HP FILTER
-#
-#---------------------------
+#################################################
+#                                               #    
+#    Excerice on Hodrick–Prescott (HP) filter   #
+#    pag. 252                                   #
+#                                               #
+#################################################
 
-
-# Clean workspace
-
-rm(list = ls(all=TRUE))
-
-# Pacchetti
-
+# Load libraries
 library(forecast)
 library(FinTS)
 library(haven)
@@ -26,47 +21,31 @@ library(ggplot2)
 library(gridExtra)
 library(reshape2)
 
+# Import data and organize it
+dataset <- read_sas("/your_path/panel.sas7bdat")
+time = dataset[,1]
 
-# Dati
-
-Dati <- read_sas("/home/giovanni/Scrivania/SASDatasets/panel.sas7bdat")
-View(Dati)
-Time = Dati[,1]
-
-
-# Dati USA
-
-USA = Dati[,9]
+# USA data
+USA = dataset[,9]
 USA = ts(USA)
-
 HPUSA = hpfilter(USA, type="lambda")
 HPUSA
-
 objectList = list(HPUSA$x,HPUSA$trend,HPUSA$cycle)
 names(objectList) = c("HPUSA","trend","cycle")
-
 sapply(objectList,class)
 
-
-#Conversion from ts to data.frame:
-
+#Conversion from ts to data.frame
 fn_ts_to_DF = function(x)  {
-  
   DF = data.frame(date=zoo::as.Date(time(objectList[[x]])),tseries=as.matrix(objectList[[x]])) 
   colnames(DF)[2]=names(objectList)[x]
   return(DF)
 }
-
-
-DFList=lapply(seq_along(objectList),fn_ts_to_DF)
+DFList = lapply(seq_along(objectList),fn_ts_to_DF)
 names(DFList) = c("HPUSA","trend","cycle")
-
 seriesTrend = merge(DFList$HPUSA,DFList$trend,by="date")
 cycleSeries = DFList$cycle
 
-
 # Plots
-
 gSeries = ggplot(melt(seriesTrend,"date"),aes(x=date,y=value,color=variable)) + geom_line() +
           ggtitle('Hodrick-Prescot Filter for HPUSA') + 
           theme(legend.title = element_blank(),legend.justification = c(0.1, 0.8), legend.position = c(0, 1),
