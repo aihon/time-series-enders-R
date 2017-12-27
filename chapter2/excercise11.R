21d0ab9af15b0e63161cb09c91316dd0f8afc6d1
aihon <34867245+aihon@users.noreply.github.com>
2017-12-27 14:39:27 +0100

    Update excercise11.R

--- chapter2/excercise11.R
+++ chapter2/excercise11.R
@@ -55,7 +55,6 @@ arOneThree # AIC = -1147.94, BIC= -1134.54
 # three lags).
 arThree <- Arima(logDiffPPI, order = c(3,0,0), fixed=c(NA,NA,NA,NA), transform.pars = FALSE)
 arThree # AIC = -1150.93, BIC= -1134.46
-
 armaOneOne <- Arima(logDiffPPI[5:211,], order = c(1,0,1), fixed=c(NA,NA,NA), transform.pars = FALSE)
 armaOneOne # AIC = -1125.39, BIC= -1112.06
 
@@ -72,7 +71,7 @@ armaOneOne # AIC = -1125.39, BIC= -1112.06
 for (i in 1:51) {
   model <- arima(window(logDiffPPI, end=159+i),order=c(3,0,0),fixed=c(NA,0,NA,NA), transform.pars = FALSE)
   prediction <- predict(mod,n.ahead=1)
-  error[i] <- pre$prediction - logDiffPPI[159+i+1]
+  error[i] <- prediction$pre - logDiffPPI[159+i+1]
 }
 errorAr = error
 
@@ -80,7 +79,7 @@ errorAr = error
 for (i in 1:51) {
   model <- arima(logDiffPPI[5:211,], order = c(1,0,1), fixed=c(NA,NA,NA), transform.pars = FALSE)
   prediction <- predict(mod,n.ahead=1)
-  error[i] <- pre$prediction - logDiffPPI[159+i+1]
+  error[i] <- prediction$pre - logDiffPPI[159+i+1]
 }
 errorArima = error
 
